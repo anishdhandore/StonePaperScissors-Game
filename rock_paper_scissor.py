11798f555b06bd50fcd81a4e17691c0ff7761ec2
anishdhandore <67917438+anishdhandore@users.noreply.github.com>
2020-08-21 21:35:24 +0530

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,59 @@
+import random
+import math
+
+mylist = ["rock", "paper",  "scissor"]
+
+run = True
+while run:
+
+    player = input("Rock/Paper/Scissor? ")
+    item = random.choice(mylist)
+
+    if player.lower() == item:
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("IT IS A TIE!")
+    # conditions when player wins
+    elif player.lower() == "rock" and item == "scissor":
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("YOU WIN!")
+
+    elif player.lower() == "paper" and item == "rock":
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("YOU WIN!")
+
+    elif player.lower() == "scissor" and item == "paper":
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("YOU WIN!")
+    # conditions when player loses
+    elif player.lower() == "rock" and item == "paper":
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("YOU LOSE!")
+
+    elif player.lower() == "paper" and item == "scissor":
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("YOU LOSE!")
+
+    elif player.lower() == "scissor" and item == "rock":
+        print("Player : {}".format(player))
+        print("Computer : {}".format(item))
+        print("YOU LOSE!")
+
+    else:
+        print("Enter only rock/paper/scissor?")
+        play_again = input("Do you want to play again, yes or no? ")
+
+        if play_again.lower() == "yes":
+            continue
+        else:
+            break
+    
+        
+
+        
+
